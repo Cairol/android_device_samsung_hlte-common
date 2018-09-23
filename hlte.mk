021809be67cde212e3cfe05c912989da824cfa2b
Cairol <Cairol75@gmx.de>
2018-09-23 20:40:23 +0200

    Add RR overlay

--- hlte.mk
+++ hlte.mk
@@ -21,6 +21,7 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-rr
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
